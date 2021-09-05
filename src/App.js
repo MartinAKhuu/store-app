a7d7cb32c41761dbe4f0c90f98e092ccbef4c9ba
Martin Khuu <martinakhuu@gmail.com>
2021-09-05 13:07:51 -0400

    Adding loader to show before we get the data from our API call

--- src/App.js
+++ src/App.js
@@ -2,14 +2,24 @@ import React, { useEffect, useState } from 'react';
 import Header from './components/Header';
 import Products from './components/Products';
 
+import ReactLoading from 'react-loading';
+
 const App = () => {
 
   const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const loaderType = "spin";
+  const loaderColor = "#333";
 
   const fetchData = async () => {
+    setLoading(true);
     await fetch("https://fakestoreapi.com/products")
       .then((response) => response.json())
-      .then((json) => setProducts(json))
+      .then(function (json) {
+        setProducts(json);
+        setLoading(false);
+      })
       .catch((error) => console.log(error))
   }
 
@@ -19,8 +29,12 @@ const App = () => {
 
   return (
     <>
-      <Header />
-      <Products products={products} />
+      {loading
+        ? <ReactLoading className="react-loader" color={loaderColor} type={loaderType} />
+        : <>
+          <Header />
+          <Products products={products} />
+        </>}
     </>
   )
 }
